299a3cea537d5c8fa961847e61803b110111a936
Robert Vinluan <robert.vinluan@nytimes.com>
2016-06-02 15:30:56 -0400

    woo

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ function map(n, start1, stop1, start2, stop2) {
 }
 
 var scene, camera, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, texture, bgReference;
 var childGeometry, childMaterial, childMesh;
 var pointsGeometry, pointsMaterial, points, pointsArray;
 
-var maxPoints = 100;
+var maxPoints = 200;
+var viewPortWidth = 1024;
+var viewPortHeight = 512;
 
 init();
 animate();
@@ -16,20 +18,32 @@ function init() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 75, viewPortWidth / viewPortHeight, 1, 10000 );
     camera.position.z = 1000;
 
-    geometry = new THREE.SphereGeometry( 100, 30, 30 );
-    material = new THREE.MeshBasicMaterial({color: 0x222222});
+    geometry = new THREE.SphereGeometry( 150, 30, 30 );
+    texture = new THREE.TextureLoader().load( "texture.png" );
+    bgReference = new THREE.TextureLoader().load( "bg.jpg" );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    material = new THREE.ShaderMaterial( {
+        uniforms: {
+            color:     { type: "c", value: new THREE.Color( 0xffffff ) },
+            texture:   { type: "t", value: texture },
+            bg_texture:   { type: "t", value: bgReference }
+        },
+        vertexShader: document.getElementById( 'orbvertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'orbfragmentshader' ).textContent,
+        transparent:    true
+    });
 
     mesh = new THREE.Mesh( geometry, material );
-    mesh.receiveShadow = true;
     scene.add( mesh );
 
     childGeometry = new THREE.BoxGeometry( 0, 0, 0 );
     childMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
     childMesh = new THREE.Mesh( childGeometry, childMaterial );
-    childMesh.position.z = 120;
+    childMesh.position.z = 240;
     mesh.add(childMesh);
 
     pointsGeometry = new THREE.BufferGeometry();
@@ -61,14 +75,25 @@ function init() {
     pointsGeometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
     pointsGeometry.addAttribute( 'ca', new THREE.BufferAttribute( colors, 4 ) );
     points = new THREE.Points(pointsGeometry, pointsMaterial);
-    points.castShadow = true;
     scene.add( points );
 
     renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( viewPortWidth, viewPortHeight );
+    renderer.domElement.width = viewPortWidth;
+    renderer.domElement.height = viewPortHeight;
+    renderer.updateStyle = true;
 
     document.body.appendChild( renderer.domElement );
 }
+
+document.getElementsByTagName('CANVAS')[0].addEventListener('mousemove', function (e) {
+    var mouse3D = new THREE.Vector3(
+            ( e.clientX / renderer.domElement.width ) * 2 - 1,
+            - ( e.clientY / renderer.domElement.height ) * 2 + 1,
+            1.0
+            );
+    // mesh.lookAt( mouse3D );
+})
 
 function updatePoints() {
     scene.updateMatrixWorld();
@@ -92,11 +117,9 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.02;
-    mesh.rotation.y += 0.04;
-    mesh.rotation.z += 0.04;
-    childMesh.rotation.y += 0.02;
-    childMesh.rotation.x += 0.02;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.03;
+    mesh.rotation.z += 0.02;
     updatePoints();
 
     renderer.render( scene, camera );
